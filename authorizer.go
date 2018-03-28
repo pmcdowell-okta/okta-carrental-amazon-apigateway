f4a5a8eb3cc562e1b27773395dbd2a170b7b9369
patrick <pmcdowell@okta.com>
2018-03-28 19:55:03 -0400

    update

--- authorizer.go
+++ authorizer.go
@@ -1,14 +1,40 @@
 package main
 
 import (
+	//"fmt"
+	"github.com/pmcdowell-okta/oktajwt"
 	"context"
-	"errors"
-	"strings"
-
+	//"errors"
+	//"strings"
+	//
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/pmcdowell-okta/oktajwt"
+	"strings"
 )
+
+func main() {
+	lambda.Start(handleRequest)
+}
+
+func handleRequest(ctx context.Context, event events.APIGatewayCustomAuthorizerRequest) (events.APIGatewayCustomAuthorizerResponse, error) {
+
+	token := string( event.AuthorizationToken)
+
+	//token := `eyJraWQiOiJkZFBVRER5VXBIMk41d0dTWHZucVFaeS1PbVRGU1Z1NVBZYW5zanBzb0FzIiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiIwMHUxOGVlaHUzNDlhUzJ5WDFkOCIsIm5hbWUiOiJva3RhcHJveHkgb2t0YXByb3h5IiwidmVyIjoxLCJpc3MiOiJodHRwczovL2NvbXBhbnl4Lm9rdGEuY29tIiwiYXVkIjoidlpWNkNwOHJuNWx4ck45YVo2ODgiLCJpYXQiOjE1MjIwOTM1NDQsImV4cCI6MTUyMjA5NzE0NCwianRpIjoiSUQuSEdiV1ZxWmJ0bFY1NUhDcFM4MlFHSEVmaVhGMWIxY0JGZE1ORVI5NF9vUSIsImFtciI6WyJwd2QiXSwiaWRwIjoiMDBveTc0YzBnd0hOWE1SSkJGUkkiLCJub25jZSI6Im4tMFM2X1d6QTJNaiIsInByZWZlcnJlZF91c2VybmFtZSI6Im9rdGFwcm94eUBva3RhLmNvbSIsImF1dGhfdGltZSI6MTUyMjA4MzkyNiwiYXRfaGFzaCI6IjZzZFNZWktUV3hkVFFMdXVLbkswU1EifQ.MGt2L3IGalqt0sd4wXYrHjgTG6Hly8IvgDvwZbZ40y-9OjhZBqkZduSboif2eqvocs66gJmJs9U4Gy1lcWTuEhJu4Hhb7nNsKEniuiv6dCt1L97OrSAMJjAvFm324Tp1kbgGocudX34CHx3RwBK0nWK8sfYgUo2_Ue7Zj0yTDSGN7qJ6640NVvxICp0QepLLXaPdSJSerqaisN8yZNhGnSrab5u347YBf-UfgLQ-4ewbeqcjhS_HyBg0zAhvzfZm52XTO4BovEx02dMSN6u_KNuprjaTT0jm3_11ldpagTujlPw6JwC1WRj-xzzffWWCAUtqWPT_V9X3OhWXEqEh-A`
+
+	token = strings.Replace(token,"Bearer ", "", -1	)
+	token = strings.Replace(token,"bearer ", "", -1	)
+
+	result, err:=oktajwt.Oktaparsejwt( token )
+	_=err
+	if result!=nil { //Bad token
+		return generatePolicy("user", "Deny", "*"), nil
+
+	} else { //Good Token
+		return generatePolicy("user", "Allow", "*"), nil
+	}
+
+}
 
 // Help function to generate an IAM policy
 func generatePolicy(principalId, effect, resource string) events.APIGatewayCustomAuthorizerResponse {
@@ -27,42 +53,6 @@ func generatePolicy(principalId, effect, resource string) events.APIGatewayCusto
 		}
 	}
 
-	// Optional output with custom properties of the String, Number or Boolean type.
-	//authResponse.Context = map[string]interface{}{
-		//"stringKey":  "stringval",
-		//"numberKey":  123,
-		//"booleanKey": true,
-	//}
 	return authResponse
 }
 
-func handleRequest(ctx context.Context, event events.APIGatewayCustomAuthorizerRequest) (events.APIGatewayCustomAuthorizerResponse, error) {
-	token := event.AuthorizationToken
-
-	result, err:=oktajwt.Oktaparsejwt ( token )
-
-	_=err
-
-	if result!=nil {
-		token="allow"
-
-	} else {
-		token="deny"
-
-	}
-
-	switch strings.ToLower(token) {
-	case "allow":
-		return generatePolicy("user", "Allow", "*"), nil
-	case "deny":
-		return generatePolicy("user", "Deny", event.MethodArn), nil
-	case "unauthorized":
-		return events.APIGatewayCustomAuthorizerResponse{}, errors.New("Unauthorized") // Return a 401 Unauthorized response
-	default:
-		return events.APIGatewayCustomAuthorizerResponse{}, errors.New("Error: Invalid token")
-	}
-}
-
-func main() {
-	lambda.Start(handleRequest)
-}
